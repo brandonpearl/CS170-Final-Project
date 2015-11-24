be51d12091a8c596be5ca7bc490ec5451847428e
Matthew Borchardt <mattcb.mml@gmail.com>
2015-11-23 20:27:27 -0800

    made classes out of AdjMatrix and AdjList

--- matt/inputTest.cpp
+++ matt/inputTest.cpp
@@ -6,33 +6,48 @@
 
 using namespace std;
 
-
 int main(int argc, char *argv[]) {
-    processInput(argc, argv);
-    adjMatrix adjM = getAdjMatrix();
-    adjList adjL = getAdjList();
+    if (argc != 2) {
+        printf("Correct usage is: ./inputTest <filepath>\n"); 
+        return 0;
+    }
+    char* filename = argv[1];
+    AdjMatrix adjM (filename);
+    AdjList adjL (filename);
 
-    int size = adjM.size;
+    int size = adjM.getSize();
 
     // Prints out the adjacency matrix (to check)
     printf("The adjancency matrix is:\n");
     for (int i=0; i < size; i++) {
         for (int j=0; j < size; j++) {
-            printf("%d ", adjM.matrix[i][j]);
+            if (adjM.edgeExists(i,j)){
+                printf("1 ");
+            }else{
+                printf("0 ");
+            }
         }
         printf("\n");
     }
 
     printf("\n");
 
+    // makes sure adjancencies in list and matrix follow
+    assert(adjM.edgeExists(0,6));
+    assert(!adjM.edgeExists(9,1));
+    assert(adjM.edgeExists(4,7));
+    assert(!adjM.edgeExists(5,5));
+    assert(adjM.edgeExists(8,2));
+
+
     // Prints out the adjaceny list (to check)
     printf("The adjancency list is:\n");
-    set<int> s;
     for (int i=0; i < size; i++) {
         printf("%d ", i);
-        s = adjL.list[i];
-        for (set<int>::iterator iter = s.begin(); iter != s.end(); iter++) {
-            printf("->%d", *iter);
+        for (int j=0; j < size; j++) {
+            if (adjL.edgeExists(i,j)) {
+                printf("->%d", j);
+            }
         }
         printf("\n");
     }
--- matt/processInput.h
+++ matt/processInput.h
@@ -4,94 +4,117 @@
 using namespace std;
 
 /* Adjacency Matrix structure
- * - Size: returns the number of nodes in the graph
- * - Matrix: if matrix[i][j] is 1, node i has an edge to node j
- *           if matrix[i][j] is 0, no edge exists between i and j
+ * - Constructor: to make a new instance called foo, use:
+ *      AdjMatrix foo (filename);
+ * - getSize(): returns the number of nodes in the graph
+ * - edgeExists(i,j): returns TRUE if edge (i,j) exists, FALSE otherwise
  */
-struct adjMatrix {
+class AdjMatrix {
     int size;
     int** matrix;
-} ;
 
-/* Adjanceny List structure
- * - Size: returns the number of nodes in the graph
- * - List: list[i] will return a set containing all nodes i has an 
- *         edge to. The nodes will be represented by their int values.
- *         To check if edge (i,j) exists, use:
- *              <adjListName>.edgeExists(i,j)
- *         Which will evaluate to TRUE if edge (i,j) exists
- */
-struct adjList {
-    int size;
-    set<int>* list;
-
-    bool edgeExists(int i, int j) {
-        return list[i].find(j) != list[i].end();
-    }
-} ;
-
-adjMatrix adjM;
-adjList adjL;
-
-int processInput(int argc, char *argv[]) {
-    // Make sure we've properly formatted our input
-    if (argc != 2) {
-        cout<<"Usage: "<< argv[0] <<" <filename>\n";
-        return 0;
-    } else {
-
+  public:
+    AdjMatrix(char* filename) {
         // Attempt to open the file
         ifstream inFile;
-        inFile.open(argv[1]);
+        inFile.open(filename);
         if (!inFile) {
             cout<<"Could not find file"<<endl;
         }
-
+    
         // Start parsing the file, assuming its properly formatted
         // We begin by getting the number of nodes (between 0 and 100)
         char line[3];
-        int size;
         inFile.getline(line, 3);
+    
         size = atoi(line);
-
-        // Intialize our Adjancency Matrix
-        adjM.size = size;
-        adjM.matrix = new int*[size];
-
-        // Initialize our Adjacency List
-        adjL.size = size;
-        adjL.list = new set<int>[size];
-
-        // We then construct our adjacency matrix and list
+        matrix = new int*[size];
+    
+        // We then construct our adjacency matrix
         char ch;
         for (int i=0; i<size; i++) {
-            adjM.matrix[i] = new int[size];
+            matrix[i] = new int[size];
             for (int j=0; j<size; j++) {
                 inFile.get(ch);
-
+    
                 // Insert into adjacency matrix
                 int e = ch - '0';
-                adjM.matrix[i][j] = e;
+                matrix[i][j] = e;
+    
+                inFile.get(ch);
+            }
+        }
+    }   
 
-                // Insert into adjacency list if edge exists
+    int getSize(){
+        return size;
+    }
+
+    bool edgeExists(int i, int j){
+        return matrix[i][j] == 1;
+    }
+} ;
+
+
+
+
+/* Adjanceny List structure
+ * - Constructor: to make a new instance called foo, use:
+ *      AdjList foo (filename);
+ * - getSize(): returns the number of nodes in the graph
+ * - edgeExists(i, j): returns TRUE if edge (i,j) exists, FALSE otherwise
+ * - allEdges(i): returns a set<int> of all edges coming from node i
+ */
+class AdjList {
+    int size;
+    set<int>* list;
+
+  public:
+    AdjList (char* filename){
+        // Attempt to open the file
+        ifstream inFile;
+        inFile.open(filename);
+        if (!inFile) {
+            cout<<"Could not find file"<<endl;
+        }
+    
+        // Start parsing the file, assuming its properly formatted
+        // We begin by getting the number of nodes (between 0 and 100)
+        char line[3];
+        inFile.getline(line, 3);
+    
+        size = atoi(line);
+        list = new set<int>[size];
+    
+        // We then construct our adjacency list
+        char ch;
+        for (int i=0; i<size; i++) {
+            for (int j=0; j<size; j++) {
+                inFile.get(ch);
+    
+                int e = ch - '0';
                 if (e == 1) {
-                    adjL.list[i].insert(j);
+                    list[i].insert(j);
                 }
                 
                 inFile.get(ch);
             }
         }
-
-        return 0;
     }
-}
 
-adjMatrix getAdjMatrix() {
-    return adjM;
-}
 
-adjList getAdjList() {
-    return adjL;
-}
+    int getSize() {
+        return size;
+    }
+
+    bool edgeExists(int i, int j) {
+        return list[i].find(j) != list[i].end();
+    }
+
+    set<int> allEdges(int i) {
+        set<int> copy (list[i]);
+        return copy;
+    }
+} ;
 
 
