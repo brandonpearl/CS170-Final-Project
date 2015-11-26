f8b50c5631b6ae3c22002e851a318827073b5e69
Ibrahim Hamisu <Senshi@Ibrahims-MacBook-Air.local>
2015-11-25 23:51:48 -0800

    main func that'll do all the fun stuff initial commit

---
+++ MAS.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "matt/processinput.h"
+#include "solutionScore.cpp"
+#include <vector>
+#include "solver_josh.cpp"
+
+using namespace std;
+
+int main(int argc, char *argv[]){
+
+}
