29383bcc6ba0f1f70226ac2245d7959810edc6d7
Josh Don <joshua@uir.com>
2015-12-02 14:17:38 -0800

    code for part1 submission

---
+++ part_1_submission/solver_brandon.cpp
@@ -0,0 +1,289 @@
+#ifndef _io
+#define _io
+#include <iostream>
+#include <assert.h>
+#include <stdio.h>
+#endif
+#ifndef _util
+#define _util
+#include "solutionScore.h"
+#include <stdlib.h>
+#include <limits>
+#endif
+#ifndef _vector
+#define _vector
+#include <vector>
+#endif
+#ifndef _adj
+#define _adj
+#include "matt/processinput.h"
+#endif
+#include <time.h>
+
+using namespace std;
+
+int swapAttempts = 10000;
+float pointless = 1e-7;
+
+class IMC {
+	/*
+	 * Simple container class for greedyInMinusOut.
+	 */
+public:
+	IMC(int player, int value) {p = player; v = value;}
+	int p;
+	int v;
+};
+
+void swap(vector<int> rankStart, AdjList adj, int swapAttempts) {
+	/*
+	 * Attempt a number of swaps equal to swapAttempts to improve the score
+	 * of rankStart.
+	 */ 
+	srand(time(NULL));
+	int prevScore = scoreSolution(rankStart, adj);
+	for (int i=0; i<swapAttempts; i++) {
+		int index1 = 0;
+		int index2 = 0;
+		while (index1 == index2) {
+			index1 = rand() % rankStart.size();
+			index2 = rand() % rankStart.size();
+		}
+		int temp = rankStart[index1];
+		rankStart[index1] = rankStart[index2];
+		rankStart[index2] = temp;
+		int testScore = scoreSolution(rankStart, adj);
+		if (testScore > prevScore) {
+			prevScore = testScore;
+		} else {
+			temp = rankStart[index1];
+			rankStart[index1] = rankStart[index2];
+			rankStart[index2] = temp;
+		}
+	}
+}
+
+vector<int> makeRandomOrder(int elements) {
+	/*
+	 * Generate a random ordering of S = {1...elements}.
+	 * Makes use of Fisher-Yates algorithm.
+	 */
+	srand(time(NULL));
+	vector<int> returnVector;
+	returnVector.reserve(elements);
+	for (int i=1; i<=elements; i++) {
+		returnVector.push_back(i);
+	}
+	for (int i=returnVector.size()-1; i>0; i--) {
+		int j = rand() % i;
+		int temp = returnVector[i];
+		returnVector[i] = returnVector[j];
+		returnVector[j] = temp;
+	}
+	return returnVector;
+}
+
+/*vector<int> frugalRoutine() {
+
+}*/
+
+/*vector<int> frugalDynamic(AdjMatrix matrix, AdjList list, int subSize) {
+
+
+	
+}*/
+
+vector<IMC> mergeSort(vector<IMC> list, int first, int last) {
+	/*
+	 * Basic merge sort algorithm in which IMC instances hold both player id's and values.
+	 */
+	if (last-first == 0) {
+		vector<IMC> ret;
+		ret.push_back(list[first]);
+		return ret;
+	} else if (last-first == 1) {
+		vector<IMC> ret;
+		ret.reserve(2);
+		if (list[first].v >= list[last].v) {
+			ret.push_back(list[last]);
+			ret.push_back(list[first]);
+		} else {
+			ret.push_back(list[first]);
+			ret.push_back(list[last]);
+		}
+		return ret;
+	} else {
+		int half = int((first+last)/2);
+		vector<IMC> left = mergeSort(list, first, half);
+		vector<IMC> right = mergeSort(list, half+1, last);
+		vector<IMC> returnVector;
+		returnVector.reserve(left.size()+right.size());
+		int i = 0;
+		int j = 0;
+		while (i < left.size() || j < right.size()) {
+			if (j >= right.size()) {
+				returnVector.push_back(left[i]);
+				i++;
+			} else if (i >= left.size()) {
+				returnVector.push_back(right[j]);
+				j++;
+			} else if (left[i].v <= right[j].v) {
+				returnVector.push_back(left[i]);
+				i++;
+			} else {
+				returnVector.push_back(right[j]);
+				j++;
+			}
+
+		}
+		return returnVector;
+	}
+}
+
+bool find(vector<int> list, int val) {
+	for (int i=0; i<list.size(); i++) {
+		if (list[i] == val) {
+			return true;
+		}
+	}
+	return false;
+}
+
+vector<int> bruteForce(vector<int> avoid, AdjList list) {
+	assert(list.getSize() <= 20);
+	if (avoid.size() == list.getSize()) {
+		return avoid;
+	}
+	vector<int> result;
+	vector<int> bestSolution;
+	int bestScore = numeric_limits<int>::min();
+	for (int i=1; i<=list.getSize(); i++) {
+		if (find(avoid, i)) {
+			//cout << "good" << endl;
+			continue;
+		}
+		vector<int> start (avoid);
+		start.push_back(i);
+		//cout << start.size() << endl;
+		result = bruteForce(start, list);
+		int testScore = scoreSolution(result, list);
+		if (testScore > bestScore) {
+			bestScore = testScore;
+			bestSolution = result;
+		}
+		result.clear();
+	}
+	return bestSolution;
+}
+
+vector<int> greedyInMinusOut(AdjMatrix matrix) {
+	/*
+	 * Sorts the nodes based upon the least to greatest ordering of indegree minus outdegree.
+	 */
+	vector<IMC> sort;
+	sort.reserve(matrix.getSize());
+	set<int> empty;
+	for (int i=0; i<matrix.getSize(); i++) {
+		sort.push_back(IMC(i+1, matrix.countIn(i, empty) - matrix.countOut(i, empty)));
+	}
+	sort = mergeSort(sort, 0, sort.size()-1);
+	vector<int> returnVector;
+	returnVector.reserve(sort.size());
+	for (int i=0; i<sort.size(); i++) {
+		returnVector.push_back(sort[i].p);
+	}
+	return returnVector;
+}
+
+vector<int> topologicalSort(AdjMatrix matrix) {
+	/*
+	 * Attempt to find optimal solution in a topological sort. If found, we will return
+	 * the vector with all elements in their topological sort. If not all edges have been
+	 * removed from the graph copy, we clear the L vector and return said empty vector.   
+	 */
+	AdjMatrix copy = matrix.duplicate();
+	vector<int> L;
+	L.reserve(copy.getSize());
+	vector<int> S;
+	set<int> empty;
+	for (int i=0; i<copy.getSize(); i++) {
+		if (copy.countIn(i, empty) == 0) {
+			S.push_back(i+1);
+		}
+	}
+	if (S.size() == 0) {
+		return L;
+	} else {
+		while (S.size() != 0) {
+			int node = S.back()-1;
+			S.pop_back();
+			L.push_back(node+1);
+			for (int i=0; i<copy.getSize(); i++) {
+				if (copy.edgeExists(node, i) && node != i) {
+					copy.removeEdge(node, i);
+					if (copy.countIn(i, empty) == 0) {
+						S.push_back(i+1);
+					}
+				}
+			}
+		}
+	}
+	if (copy.hasEdges()) {
+		L.clear();
+		return L;
+	} else {
+		return L;
+	}
+}
+
+vector<int> solve_instance_brandon(AdjMatrix matrix, AdjList list) {
+	vector<int> best;
+	//return bruteForce(best, list);
+	vector<int> random = makeRandomOrder(matrix.getSize());
+	int currentScore = scoreSolution(random, list);
+	float percentChange = numeric_limits<float>::max();
+	int stagnant = 0;
+	cout << "Beginning Random Swaps" << endl;
+	while (abs(percentChange) > pointless && stagnant <= 100) {
+		swap(random, list, swapAttempts);
+		int nextScore = scoreSolution(random, list);
+		percentChange = float(((float)nextScore - (float)currentScore)/(float)currentScore);
+		if (percentChange <= pointless) {
+			stagnant++;
+		} else {
+			stagnant = 0;
+		}
+		currentScore = nextScore;
+	}
+	cout << "Attempting Topological Sort" << endl;
+	vector<int> topological = topologicalSort(matrix);
+	cout << "Beginning Greedy Approximation" << endl;
+	vector<int> greedy = greedyInMinusOut(matrix);
+	if (topological.size() != 0) {
+		return topological;
+	}
+	vector<vector<int> > solutionCandidates;
+	solutionCandidates.push_back(random);
+	solutionCandidates.push_back(greedy);
+	int bestScore = numeric_limits<int>::min();
+	vector<int> bestSolution;
+	for (int i=0; i<solutionCandidates.size(); i++) {
+		int testScore = scoreSolution(solutionCandidates[i], list);
+		if (testScore > bestScore) {
+			bestScore = testScore;
+			bestSolution = solutionCandidates[i];
+		}
+	}
+	swap(bestSolution, list, swapAttempts);
+	return bestSolution;
+
+}
+
+int main(int argc, char *argv[]) {
+	AdjMatrix matrix (argv[1]);
+    AdjList list (argv[1]);
+
+    vector<int> solution = solve_instance_brandon(matrix, list);
+    cout << scoreSolution(solution, list);
+    return 0;
+}
---
+++ part_1_submission/solver_matt.cpp
@@ -0,0 +1,389 @@
+#include <vector>
+#include <iostream>
+#include <random>
+#include <chrono>
+#include <list>
+#include <stack>
+// #include "matt/processInput.h"
+#include "solutionScore.h"
+// #include "solver_josh.cpp"
+using namespace std;
+
+//========================
+//====== UTILITIES =======
+//========================
+
+int matt_score_ordering(int *ordering, AdjList list) {
+    std::vector<int> v(ordering, ordering + list.getSize());
+    return scoreSolution(v, list);
+}
+
+void matt_print_array(int *inp, int size) {
+    printf("[");
+    int  count=0;
+    for (int i=0; i<size; i++){
+        printf(" %d", inp[i]);
+        count++;
+    }
+    printf("]\n");
+    printf("size is: %d\n", count);
+}
+
+// Initializes vertex array by picking a random order and seeing whether
+// it or its reverse returns a better score. Will set inp to be whichever
+// one returns the higher score
+void initialize_vertex_array(int *inp, int size, AdjList list) {
+    for (int i=0; i < size; i++) {
+        inp[i] = i+1;
+    }
+    std::vector<int> v(inp, inp + size);
+
+    unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
+    auto gen = std::default_random_engine(seed);
+    shuffle(v.begin(), v.end(), gen);
+
+    // Pick either this or reverse. Whichever is bigger
+    int defScore = scoreSolution(v, list);
+    std::vector<int> copy(v);
+    for (int i=0; i<size; i++) {
+        copy[i] = v[size-i-1];
+    }
+    int revScore = scoreSolution(copy, list);
+    if (revScore > defScore){
+        v = copy;
+    }
+
+    // Print this array out and set inp equal to it
+    // printf("Naive ordering is: [");
+
+    for (int i=0; i < size; i++) {
+        // printf("%d, ", v[i]);
+        inp[i] = v[i];
+    }
+    // printf("]\n");
+}
+
+// Copies array "from" to "to", where both arrays have "size" size
+void copyIntArray (int* to, int* from, int size) {
+    for (int i=0; i<size; i++) {
+        to[i] = from[i];
+    }
+}
+
+
+//========================
+//====== ALGORITHMS ======
+//========================
+
+// orders nodes according to the following criteria:
+//  - Minimum total incoming nodes is first criteria
+//  - If tied, max incoming from nodes already visited
+// Returns an int* with this ordering
+void greedyIncoming(AdjMatrix matrix, int* inp, int size) {
+    int min;
+    int indexToAdd;
+    set<int> ignore;
+    set<int> notIgnore;
+    for (int i=0; i<size; i++) {
+        notIgnore.insert(i);
+    }
+
+    for (int r=0; r<size; r++) { // makes sure we repeat
+        min = 1000;
+        indexToAdd = 0;
+        for (int i=0; i<size; i++) {
+            if (ignore.find(i) == ignore.end()) { // if not on our ignore list
+                if (matrix.countIn(i, ignore) < min) {
+                    min = matrix.countIn(i, ignore);
+                    indexToAdd = i+1;
+                } else if (matrix.countIn(i, ignore) == min) {
+                    int newI = matrix.countOut(i, notIgnore);
+                    int oldI = matrix.countOut(indexToAdd, notIgnore);
+                    if (newI > oldI) {
+                        indexToAdd = i+1;
+                    }
+                }
+            }
+        }
+        inp[r] = indexToAdd;
+        ignore.insert(indexToAdd-1);
+        notIgnore.erase(indexToAdd-1);
+    }
+}
+
+// orders nodes according to the following criteria:
+//  - Maximum total outgoing nodes is first criteria
+//  - If tied, max incoming from nodes already visited
+// Returns an int* with this ordering
+void greedyOutgoing(AdjMatrix matrix, int* inp, int size) {
+    int max;
+    int indexToAdd;
+    set<int> ignore;
+    set<int> notIgnore;
+    for (int i=0; i<size; i++) {
+        notIgnore.insert(i);
+    }
+
+    for (int r=0; r<size; r++) { // makes sure we repeat
+        max = -1;
+        indexToAdd = 0;
+        for (int i=0; i<size; i++) {
+            if (ignore.find(i) == ignore.end()) { // if not on our ignore list
+                if (matrix.countOut(i, ignore) > max) {
+                    max = matrix.countOut(i, ignore);
+                    indexToAdd = i+1;
+                } else if (matrix.countOut(i, ignore) == max) {
+                    int newI = matrix.countIn(i, notIgnore);
+                    int oldI = matrix.countIn(indexToAdd, notIgnore);
+                    if (newI > oldI) {
+                        indexToAdd = i+1;
+                    }
+                }
+            }
+        }
+        inp[r] = indexToAdd;
+        ignore.insert(indexToAdd-1);
+        notIgnore.erase(indexToAdd-1);
+    }
+}
+
+// generates 1,000 random orders and returns the max scoring one
+void randomSolver (int* inp, int size, AdjList list) {
+    std::vector<int> v(inp, inp + size);
+    int score = scoreSolution(v, list);
+    int newScore;
+    std::vector<int> copy(v);
+    for (int i=0; i<1000; i++) {
+        std::random_shuffle(copy.begin(), copy.end());
+        newScore = scoreSolution(copy, list);
+        if (newScore > score){
+            score = newScore;
+            v = copy;
+        }
+    }
+    for (int i=0; i < size; i++) {
+        inp[i] = v[i];
+    }
+    return;
+}
+
+// Compares each pair from (1,2) to (99,100) and switches
+// if the switch will be better
+void pairSwapForward (AdjList list, int* inp, int size) {
+    std::vector<int> v(inp, inp + size);
+    int score = scoreSolution(v, list);
+    int newScore;
+    for (int i=0; i<100; i++) {
+        for (int j=i+1; j<100; j++) {
+            std::vector<int> copy(v);
+            int temp = copy[j];
+            copy[j] = copy[i];
+            copy[i] = temp;
+            newScore = scoreSolution(copy, list);
+            if (newScore > score){
+                score = newScore;
+                v = copy;
+            }
+        }
+    }
+    for (int i=0; i < size; i++) {
+        inp[i] = v[i];
+    }
+    return;
+}
+
+// Compares each pair from (99,100) to (1,2) and switches
+// if the switch will be better
+void pairSwapBackward (AdjList list, int* inp, int size) {
+    std::vector<int> v(inp, inp + size);
+    int score = scoreSolution(v, list);
+    int newScore;
+    for (int i=99; i>=0; i--) {
+        for (int j=i-1; j>=0; j--) {
+            std::vector<int> copy(v);
+            int temp = copy[j];
+            copy[j] = copy[i];
+            copy[i] = temp;
+            newScore = scoreSolution(copy, list);
+            if (newScore > score){
+                score = newScore;
+                v = copy;
+            }
+        }
+    }
+    for (int i=0; i < size; i++) {
+        inp[i] = v[i];
+    }
+    return;
+}
+
+// For each node:
+// - Run DFS starting from that node
+// - Return the nodes in topological order
+// Inspired by: http://www.geeksforgeeks.org/topological-sorting/
+
+void topologicalSortUtil(AdjList list, int i, int size, bool* visited, stack<int> &Stack){
+    // Mark the current node as visited
+    visited[i] = true;
+
+    // Recurse for all the vertices adjacent to this vertex
+    for (int n=1; n<size+1; n++){
+        if (list.edgeExists(i-1,n-1)&&(!visited[n])) {
+            topologicalSortUtil(list, n, size, visited, Stack);
+        }
+    }
+    Stack.push(i);
+    return;
+}
+
+void topologicalSort(AdjList list, int* inp, int size) {
+    stack<int> Stack;
+    int bestScore = 0;
+    for (int s=1; s<size+1; s++) {
+        int temp[size];
+        bool visited[size+1];
+
+        for (int i=1; i<size+1; i++) {
+            visited[i] = false;
+        }
+
+        for (int i=s; i<size+1; i++) {
+            if (visited[i] == false) {
+                topologicalSortUtil(list, i, size, visited, Stack);
+            }
+        }
+        for (int i=1; i<s; i++) {
+            if (visited[i] == false) {
+                topologicalSortUtil(list, i, size, visited, Stack);
+            }
+        }
+
+        int i=0;
+        while (!Stack.empty()) {
+            temp[i] = Stack.top();
+            Stack.pop();
+            i += 1;
+        }
+        int newScore = matt_score_ordering(temp, list);
+        if (newScore > bestScore){
+            bestScore = newScore;
+            for (int i=0; i<size; i++){
+                inp[i] = temp[i];
+            }
+        }
+    }
+    return;
+}
+
+//========================
+//======== SOLVER ========
+//========================
+
+std::vector<int> solve_instance_matt(AdjMatrix matrix, AdjList list) {
+    int size = matrix.getSize();
+    int vertex_array[size];
+    int best_array[size];
+    int score;
+    int newScore;
+    int naiveScore;
+    initialize_vertex_array(vertex_array, size, list);
+    copyIntArray(best_array, vertex_array, size);
+
+    cout << endl << "Starting solver_matt" << endl;
+
+    score = matt_score_ordering(vertex_array, list);
+    naiveScore = score;
+    cout << "Naive score is: " << score << endl;
+
+    //randomSolver(vertex_array, size, list);
+    //newScore = matt_score_ordering(vertex_array, list);
+    //if (newScore > score) {
+    //    copyIntArray(best_array, vertex_array, size);
+    //    score = newScore;
+    //}
+    //printf("randomSolver score is: %d\n", newScore);
+
+
+    //topologicalSort(list, vertex_array, size);
+    //newScore = matt_score_ordering(vertex_array, list);
+    //printf("successfully finished\n");
+    //if (newScore > score) {
+    //    copyIntArray(best_array, vertex_array, size);
+    //    score = newScore;
+    //}
+    //printf("topologicalSort score is: %d\n", newScore);
+
+
+    greedyIncoming(matrix, vertex_array, size);
+    newScore = matt_score_ordering(vertex_array, list);
+    if (newScore > score) {
+        copyIntArray(best_array, vertex_array, size);
+        score = newScore;
+    }
+    cout << "GreedyIncoming score is: " << newScore << endl;
+
+
+    greedyOutgoing(matrix, vertex_array, size);
+    newScore = matt_score_ordering(vertex_array, list);
+    if (newScore > score) {
+        copyIntArray(best_array, vertex_array, size);
+        score = newScore;
+    }
+    cout << "GreedyOutgoing score is: " << newScore << endl;
+
+    pairSwapForward(list, vertex_array, size);
+    newScore = matt_score_ordering(vertex_array, list);
+    if (newScore > score) {
+        copyIntArray(best_array, vertex_array, size);
+        score = newScore;
+    }
+    cout << "pairSwapForward #1 score is: " << newScore << endl;
+
+    pairSwapBackward(list, vertex_array, size);
+    newScore = matt_score_ordering(vertex_array, list);
+    if (newScore > score) {
+        copyIntArray(best_array, vertex_array, size);
+        score = newScore;
+    }
+    cout << "pairSwapBackward #1 score is: " << newScore << endl;
+
+    pairSwapForward(list, vertex_array, size);
+    newScore = matt_score_ordering(vertex_array, list);
+    if (newScore > score) {
+        copyIntArray(best_array, vertex_array, size);
+        score = newScore;
+    }
+    cout << "pairSwapForward #2 score is: " << newScore << endl;
+
+    pairSwapBackward(list, vertex_array, size);
+    newScore = matt_score_ordering(vertex_array, list);
+    if (newScore > score) {
+        copyIntArray(best_array, vertex_array, size);
+        score = newScore;
+    }
+    cout << "pairSwapBackward #2 score is: " << newScore << endl;
+
+    int scoreGain = score-naiveScore;
+    float percentGain = ((float)score - (float)naiveScore)/(float)naiveScore*(float)100;
+    cout << "Improved by " << scoreGain;
+    cout << " points, which is "<<percentGain <<" percent\n";
+
+    vector<int> best (vertex_array, vertex_array + size);
+    return best;
+}
+
+int matt_main(int argc, char *argv[]) {
+    if (argc != 2) {
+        printf("Need to supply instance filename\n");
+        exit(1);
+    }
+    AdjMatrix matrix (argv[1]);
+    AdjList list (argv[1]);
+
+    solve_instance_matt(matrix, list);
+    return 0;
+}
+
+//int main(int argc, char *argv[]) {
+//    return matt_main(argc, argv);
+//}
